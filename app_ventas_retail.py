3a2956d11e7495ed1eba9f92c5c5ad3582cde073
Rodrigo-electro <rodrigoalvarezacampos@gmail.com>
2026-01-07 21:51:00 -0600

    Update app_ventas_retail.py

--- app_ventas_retail.py
+++ app_ventas_retail.py
@@ -316,7 +316,7 @@ print("📊 Métricas calculadas:")
 # Convertir las métricas en un DataFrame para una mejor visualización
 metricas_df = pd.DataFrame(list(metricas_detalladas.items()), columns=['Métrica', 'Valor'])
 # con python en google colab funciona como display, en stream se cambia display por "st.dataframe"
-display(metricas_df)
+st.dataframe(metricas_df)
 
 """### 💡 Nota:
 MAPE(Mean Absolute Percentage Error) es especialmente útil para comunicar error a audiencias de negocio porque está en porcentaje.
